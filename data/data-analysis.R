df31b3490b1beb7293a880b20c8704c0a3e19bc7
Jessica Abt <126521042+jessicaabt@users.noreply.github.com>
2026-03-31 19:32:45 -0500

    Update data analysis

---
+++ data/data-analysis.R
@@ -0,0 +1,25 @@
+> cleaned_oasis <- read.csv("~/SDS 320e Project/cleaned_oasis.csv")
+>   View(cleaned_oasis)
+> hist(cleaned_oasis$MMSE, xlab='MMSE Score', main='Distribution of MMSE Scores', right=FALSE, col='lightblue')
+> fivenum(cleaned_oasis$MMSE)
+[1] 17.0 25.5 29.0 29.0 30.0
+> mean(cleaned_oasis$MMSE)
+[1] 27.05
+> hist(cleaned_oasis$EDUC, xlab='Years of Education', main='Distribution of Years of Education', right=FALSE, col='lightblue')
+> fivenum(cleaned_oasis$EDUC)
+[1]  6 12 14 16 23
+> mean(cleaned_oasis$EDUC)
+[1] 14.33
+> plot(cleaned_oasis$EDUC, cleaned_oasis$MMSE, xlab='Years of Education', ylab='MMSE Score', main='Years of Education and MMSE Score', pch=16)
+> cor(cleaned_oasis$EDUC, cleaned_oasis$MMSE, use = 'complete.obs')
+[1] 0.2355137
+> barplot(table(cleaned_oasis$Group), ylab='Frequency', main='Counts of Demented and Nondemented Group', col='lightblue')
+> boxplot(MMSE~Group, data=cleaned_oasis, xlab='Group', ylab='MMSE Score', main='MMSE Score Distribution Between Groups', col=c('lightblue', 'lightpink'))
+> fivenum(cleaned_oasis$MMSE[cleaned_oasis$Group == "Demented"])
+[1] 17.0 23.0 25.5 27.0 30.0
+> mean(cleaned_oasis$MMSE[cleaned_oasis$Group == "Demented"])
+[1] 24.92
+> fivenum(cleaned_oasis$MMSE[cleaned_oasis$Group == "Nondemented"])
+[1] 26 29 29 30 30
+> mean(cleaned_oasis$MMSE[cleaned_oasis$Group == "Nondemented"])
+[1] 29.18
