0ba5e491fc920065112d12b66dc371ce6d3f5358
Jessica Abt <126521042+jessicaabt@users.noreply.github.com>
2026-04-21 15:24:28 -0500

    update multiple regression analysis

--- data/data-analysis.R
+++ data/data-analysis.R
@@ -21,3 +21,41 @@
 [1] 26 29 29 30 30
 > mean(cleaned_oasis$MMSE[cleaned_oasis$Group == "Nondemented"])
 [1] 29.18
+> model <- lm(MMSE ~ EDUC + Group, data = cleaned_oasis)
+> hist(model$residuals, main = "Model Residuals", xlab = "Residual", col = "light blue", right = F)
+> plot(model$fitted.values, model$residuals, xlab = "Fitted Values", ylab = "Residuals", main = "Residual Plot", pch = 20)
+> abline(h = 0, col = "red")
+> summary(model)
+
+Call:
+lm(formula = MMSE ~ EDUC + Group, data = cleaned_oasis)
+
+Residuals:
+    Min      1Q  Median      3Q     Max 
+-7.8474 -1.0755 -0.0093  0.9760  5.1526 
+
+Coefficients:
+                 Estimate Std. Error t value
+(Intercept)      24.25863    1.27455   19.03
+EDUC              0.04906    0.09092    0.54
+GroupNondemented  4.17659    0.51821    8.06
+                 Pr(>|t|)    
+(Intercept)        <2e-16 ***
+EDUC                0.591    
+GroupNondemented    2e-12 ***
+---
+Signif. codes:  
+  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1
+  ‘ ’ 1
+
+Residual standard error: 2.473 on 97 degrees of freedom
+Multiple R-squared:  0.4343,	Adjusted R-squared:  0.4226 
+F-statistic: 37.23 on 2 and 97 DF,  p-value: 1.001e-12
+
+> summary(model)$adj.r.squared
+[1] 0.422635
+> library(ggplot2)
+> ggplot(cleaned_oasis, aes(x = EDUC, y = MMSE, col = Group, shape = Group)) + 
++     geom_point() + xlab("Years of Education") + ylab("MMSE Score") + 
++     labs(col = "Diagnostic Group", shape = "Diagnostic Group") + ggtitle("MMSE Score by Diagnostic Group and Years of Education") + 
++     theme_classic() + scale_color_manual(values = c("lightblue", "lightpink")) + geom_line(aes(y = yhat))
